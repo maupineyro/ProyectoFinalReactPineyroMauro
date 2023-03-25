f02dfe7c52ece79957ca3146c1ec72da02a54084
maupineyro <argmau357@gmail.com>
2023-03-25 16:12:45 -0300

    navbar y carwidget

---
+++ src/Components/CartWidget/CartWidget.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const CartWidget = () => {
+    return (
+        <div>
+            <li>
+                <a className="linkCarrito botonMenu" href="#">
+                    <i className="bi bi-cart-fill"></i><span id="numeroCarritoIndex"
+                        className="numeroEnCarrito">7</span>
+                </a>
+            </li>
+        </div>
+    )
+}
+
+export default CartWidget
--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,20 +1,42 @@
 import React from 'react';
 import logo from "../../logo.svg"
+import { Link } from 'react-router-dom';
+import CartWidget from '../CartWidget/CartWidget';
 
 const NavBar = () => {
     return (
         <div>
 
+            <nav className="navbar navbar-expand-lg ">
+                <div className="container-fluid">
+                    <Link to={'/'} className="navbar-brand"><img src={logo} className="App-logo" alt="logo" /> REACTIVE </Link>
+
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav d-flex ms-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <Link to={'/'} className="nav-link active" aria-current="page">Catálogo</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to={'/'} className="nav-link" aria-current="page">Categorías</Link>
+                            </li>
 
 
-            <header className="navBar">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    REACTIVE
-                </p>
+                            <li className="nav-item">
+                                <CartWidget />
+                            </li>
+
+                        </ul>
+                    </div>
+                </div>
+            </nav>
 
 
-            </header>
+
+
+
         </div>
     )
 }
